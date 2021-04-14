cb694bccf9231dafc098551c224c4f33453711cc
mclark45 <80595429+mclark45@users.noreply.github.com>
2021-04-14 19:29:25 -0400

    added in Sleeping Guard Activation

---
+++ Assets/The Great Fleece/Game/Scripts/GrabKeyCardActivation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabKeyCardActivation : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _grabCard;
+    private bool _scenePlayed;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && _scenePlayed == false)
+        {
+            _scenePlayed = true;
+            _grabCard.SetActive(true);
+        }
+    }
+}
